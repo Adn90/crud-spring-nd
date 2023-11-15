3a3cfa50f0dab357bb668080d9f36565b550f19a
adn90 <adonisjsouza@gmail.com>
2023-11-15 15:57:14 -0300

    H2 setup; JsonProperty to convert object->json with fronend atts

--- src/main/java/com/adn/model/Course.java
+++ src/main/java/com/adn/model/Course.java
@@ -1,5 +1,6 @@
 package com.adn.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 import lombok.Data;
@@ -11,6 +12,8 @@ public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    // front-end uses _id instead of back-end id. when object-json conversion occur, json now has a _id att
+    @JsonProperty("_id")
     private Long id;
 
     //@Column(name = "nome") in case where legacy database, you can make the association.
